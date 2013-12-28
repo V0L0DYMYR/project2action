b055ef1785265a4a0c21d5d642a490b59591f845
volodymyr derkach <vlde@ciklum.com>
2013-12-28 23:21:11 +0200

    rename

--- server-jar/bin/debug.sh
+++ server-jar/bin/debug.sh
@@ -1,2 +1,2 @@
 
-java  -jar -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 ../target/in-queue-1.0-SNAPSHOT.jar server ../src/main/resources/conf/in-queue.json
+java  -jar -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 ../target/project2action-1.0-SNAPSHOT.jar server ../src/main/resources/conf/project2action.json
